9b3b1c07d110764d43886155103594d37f3c6e22
ashley needy <ashleyneedy@yahoo.com>
2021-08-28 03:26:14 -0400

    subnav changes

--- JS/sideBar.js
+++ JS/sideBar.js
@@ -1,25 +1,25 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "350px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "20px";
+  document.getElementById("main").style.marginLeft = "0";
 }
 
 function openOtherNav() {
   document.getElementById("myOtherSidebar").style.width = "250px";
-  document.getElementById("main2").style.marginLeft = "240px";
-  document.getElementById("main").style.marginLeft = "-100%"; 
+  document.getElementById("main2").style.marginLeft = "250px";
+  
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeOtherNav() {
   document.getElementById("myOtherSidebar").style.width = "0";
-  document.getElementById("main2").style.marginLeft = "15px";
-  document.getElementById("main").style.marginLeft = "20px"; 
+  document.getElementById("main2").style.marginLeft = "0px";
+  
 }
 
